ac876caf568844b16b01e608c09b113edbe73a79
Evgeny Maksimenko <eugene.maksimenko@yandex.ru>
2020-06-02 23:12:57 +0300

    Add functions.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    do {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms));
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +19,48 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Фильм?', ''),
-    b = prompt('оценка', ''),
-    c = prompt('Фильм?', ''),
-    d = prompt('оценка', '');
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        let a;
+        do {
+            a = prompt('Фильм(?', '');
+        } while (!a || a == '' || a.length > 50);
+        let b;
+        do {
+            b = prompt('оценка', '');
+        } while (b != null || b == '' || b.length > 50);
+        personalMovieDB.movies[a] = b;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel() {
+if (personalMovieDB.count < 10) {
+    console.log('Довольно мало');
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >=30) {
+    console.log('Вы киноман');
+} else{
+    console.log('Ошибка');
+}
+}
+
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for(let i=1; i<=3; i++) {
+        personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+
+writeYourGenres();
+
+function showMyDb(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDb(personalMovieDB.privat);
